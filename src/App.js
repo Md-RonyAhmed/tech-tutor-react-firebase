71959dec9e93236c4dfcb07c024204eaf8758e47
Md. Rony <rony15-10527@diu.edu.bd>
2022-04-18 01:44:51 +0600

    NotFound page added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import Home from './Pages/Home/Home/Home';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
+import NotFound from './Pages/Shared/NotFound/NotFound';
 
 function App() {
   return (
     <div>
-      
       <Header></Header>
       <Home></Home>
+      <NotFound></NotFound>
       <Footer></Footer>
     </div>
   );
--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import img from '../../../Assets/Images/NotFound/404.jpg';
 
 const NotFound = () => {
    return (
       <div>
-         
+         <img src={img} alt="" />
       </div>
    );
 };
