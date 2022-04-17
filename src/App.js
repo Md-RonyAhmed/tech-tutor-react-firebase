c56876edce500829bc0521f7aade72e479cbbed3
Md. Rony <rony15-10527@diu.edu.bd>
2022-04-17 15:38:37 +0600

    tailwind css added and header component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
     <div>
-      <h1>Hello everyone</h1>
+      <Header></Header>
     </div>
   );
 }
