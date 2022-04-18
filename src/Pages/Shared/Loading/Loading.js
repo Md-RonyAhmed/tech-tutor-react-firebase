11d164c75f0464881442d7fae136d4a0c1cbe58f
Md. Rony <rony15-10527@diu.edu.bd>
2022-04-18 14:27:04 +0600

    Loading added

--- src/Pages/Shared/Loading/Loading.js
+++ src/Pages/Shared/Loading/Loading.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 const Loading = () => {
    return (
-      <div>
-         
-      </div>
+     <div class="flex items-center justify-center space-x-2">
+       <div
+         class="spinner-border animate-spin inline-block w-12 h-12 border-4 rounded-full"
+         role="status"
+       >
+         <span class="visually-hidden">Loading...</span>
+       </div>
+     </div>
    );
 };
 
