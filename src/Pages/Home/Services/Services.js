807bf5ba4ec4be175c1e490ce325bba5281bffbe
Md. Rony <rony15-10527@diu.edu.bd>
2022-04-18 12:16:48 +0600

    Services updated

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Service from '../Service/Service';
 
 const Services = () => {
-   return (
-      <div>
-         
-      </div>
-   );
+       const [services, setServices] = useState([]);
+
+       useEffect(() => {
+         fetch("services.json")
+           .then((res) => res.json())
+           .then((data) => setServices(data));
+       }, []);
+
+       return (
+         <div id="services" className="container">
+           <div className="row-span-3">
+             <h1 className="text-blue-600 text-center mt-5 text-3xl"> Our Services</h1>
+             <div className="services-container">
+               {services.map((service) => (
+                 <Service key={service.id} service={service}></Service>
+               ))}
+             </div>
+           </div>
+         </div>
+       );
 };
 
 export default Services;
