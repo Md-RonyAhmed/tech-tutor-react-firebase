9689d5093f7ff2d037cdcdca8dd5af454898f975
Md. Rony <rony15-10527@diu.edu.bd>
2022-04-18 14:39:00 +0600

    Social login updated

--- src/Pages/Authentication/SocialLogin/SocialLogin.js
+++ src/Pages/Authentication/SocialLogin/SocialLogin.js
@@ -1,7 +1,35 @@
 import React from 'react';
 import google from '../../../Assets/Images/social icon/google.png';
 import github from '../../../Assets/Images/social icon/github.png';
+import auth from '../../../firebase.init';
+import {
+  useSignInWithGithub,
+  useSignInWithGoogle,
+} from "react-firebase-hooks/auth";
+import { useNavigate } from 'react-router-dom';
+import Loading from '../../Shared/Loading/Loading';
 const SocialLogin = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const [signInWithGithub, user1, loading1, error1] = useSignInWithGithub(auth);
+  const navigate = useNavigate();
+
+  let errorElement;
+
+  if (loading || loading1) {
+    return <Loading></Loading>;
+  }
+
+  if (error || error1) {
+    errorElement = (
+      <p className="text-red-600">
+        Error: {error?.message} {error1?.message}
+      </p>
+    );
+  }
+
+  if (user || user1) {
+    navigate("/");
+  }
    return (
      <div>
        <div className="flex items-center justify-center mt-2">
@@ -9,8 +37,9 @@ const SocialLogin = () => {
          <p className="px-2">Or</p>
          <div style={{ height: "1px" }} className="bg-black w-48"></div>
        </div>
+       {errorElement}
        <div>
-         <button className="shadow-md rounded-md w-full flex mx-auto my-4 py-2 px-2 items-center justify-center hover:bg-slate-100 hover:shadow-inner">
+         <button onClick={()=>signInWithGoogle()} className="shadow-md rounded-md w-full flex mx-auto my-4 py-2 px-2 items-center justify-center hover:bg-slate-100 hover:shadow-inner">
            <img
              className="mr-2"
              style={{ width: "30px" }}
@@ -19,7 +48,7 @@ const SocialLogin = () => {
            />
            <span className="text-xl text-amber-500">Sign In with Google</span>
          </button>
-         <button className="shadow-md rounded-md w-full flex mx-auto my-4 py-2 px-2 items-center justify-center hover:bg-slate-100 hover:shadow-inner">
+         <button onClick={()=>signInWithGithub()} className="shadow-md rounded-md w-full flex mx-auto my-4 py-2 px-2 items-center justify-center hover:bg-slate-100 hover:shadow-inner">
            <img
              className="mr-2"
              style={{ width: "30px" }}
