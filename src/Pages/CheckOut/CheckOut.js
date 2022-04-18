f9f219798acfa9a068a58b4c95d9358c73de987d
Md. Rony <rony15-10527@diu.edu.bd>
2022-04-18 23:03:41 +0600

    Checkout completed

--- src/Pages/CheckOut/CheckOut.js
+++ src/Pages/CheckOut/CheckOut.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const CheckOut = () => {
    return (
-      <div>
-         <h1>Please checkout </h1>
+      <div className='text-center'>
+         <h1 className='text-4xl text-center mt-6'>Please checkout your booking services.</h1>
       </div>
    );
 };
