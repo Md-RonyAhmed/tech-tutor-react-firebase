7f2e39ad366d88425d1b844b25d66b083db16870
Md. Rony <rony15-10527@diu.edu.bd>
2022-04-18 17:45:57 +0600

    Signup and signin updated

--- src/Pages/Authentication/SignIn/SignIn.js
+++ src/Pages/Authentication/SignIn/SignIn.js
@@ -1,15 +1,64 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useRef } from 'react';
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import auth from '../../../firebase.init';
+import Loading from '../../Shared/Loading/Loading';
 import SocialLogin from '../SocialLogin/SocialLogin';
 
 const SignIn = () => {
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  let from = location.state?.from?.pathname || "/";
+  let errorElement;
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+
+  const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
+
+  if (loading || sending) {
+    return <Loading></Loading>;
+  }
+
+  if (user) {
+    navigate(from, { replace: true });
+  }
+
+  if (error) {
+    errorElement = <p className="text-danger">Error: {error?.message}</p>;
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+
+    signInWithEmailAndPassword(email, password);
+  };
+
+  const navigateRegister = (event) => {
+    navigate("/signup");
+  };
+
+  const resetPassword = async () => {
+    const email = emailRef.current.value;
+    if (email) {
+      await sendPasswordResetEmail(email);
+      toast("Sent email");
+    } else {
+      toast("Please enter your email address");
+    }
+  };
    return (
      <div className="my-16">
        <h1 className="text-blue-600 text-center text-4xl my-5">
          Please Log In
        </h1>
        <div className="block p-6 rounded-lg shadow-lg bg-white max-w-sm mx-auto">
-         <form>
+         <form onSubmit={handleSubmit}>
            <div className="form-group mb-6">
              <label
                htmlFor="exampleInputEmail2"
