9f5411010ac9f4f174a26f738ca819eab4e11ac6
Md. Rony <rony15-10527@diu.edu.bd>
2022-04-18 23:11:24 +0600

    Servicedetail completed

--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -4,11 +4,11 @@ import { Link, useParams } from "react-router-dom";
 const ServiceDetail = () => {
   const { serviceId } = useParams();
   return (
-    <div>
-      <h2>Welcome to detail: {serviceId}</h2>
+    <div className="mt-6 pt-10">
+      <h2 className="text-center text-3xl mt-11">Welcome to detail with: {serviceId}</h2>
       <div className="text-center">
         <Link to="/checkout">
-          <button className="block w-52 mx-auto px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
+          <button className="block w-52 mx-auto px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mt-4">
             Proceed Checkout
           </button>
         </Link>
